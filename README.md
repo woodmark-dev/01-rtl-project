f3d90e62b86ab4b5c3bf5531dedb4df159c8703e
Jonathan Otokun <tega_joe@hotmail.com>
2024-04-07 19:32:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 01-RTL-project
+This project is for the purpose of 01-RTL-project. This could change in the future and this description will adapt to the change.
+
+## Current Project
+A simple Wish app that accepts and display your wish
+
+## Tools Used
+Vite, React, TypeScript, React Testing Library, ViTest.
+
+## Commands
+
+1. Clone with `git@github.com:afroguy16/01-rtl-project.git`
+2. `yarn`
+3. To test `yarn test`
+4. To run in your browser `yarn dev`
---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  const [wish, setWish] = useState("");
+  const hasWish = wish.length > 0;
+  const [wishSubmitted, setWishSubmitted] = useState(false);
+
+  return !wishSubmitted ? (
+    <article>
+      <h1>Make a wish</h1>
+      <input
+        type="text"
+        aria-label="wish input"
+        placeholder="enter your wish"
+        onChange={(e) => {
+          setWish(e.target.value);
+        }}
+      />
+      <button disabled={!hasWish} onClick={() => setWishSubmitted(true)}>
+        Submit wish
+      </button>
+    </article>
+  ) : (
+    <article>
+      <h1>Your wish is</h1>
+      <p>{wish}</p>
+    </article>
+  );
+}
+
+export default App;
